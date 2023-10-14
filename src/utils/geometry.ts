50d2bce7b87e8da2b2a99c9d47513df2f984d617
Nagellan <ireknazmievirek@gmail.com>
2023-10-14 16:49:54 +0400

    feat: add threshold reduction coefficient for fov

--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -37,24 +37,32 @@ export const getYIndex = (y: Coordinate, size: number): number =>
   Math.floor(size / 2) - y;
 
 /**
+ * Степень уменьшения порога вхождения точки в окружность
+ * Образуется более «округлая» окружность из точек
+ */
+const BEAUTY_COEFFICIENT = 0.6;
+
+/**
  * Узнать, находится ли точка внутри окружности (включительно)
  *
  * @link https://math.stackexchange.com/a/198769
  *
- * @param xp координата X переданной точки
- * @param yp координата Y переданной точки
- * @param xc координата X центра окружности
- * @param yc координата Y центра окружности
- * @param r  радиус окружности
+ * @param xp                 координата X переданной точки
+ * @param yp                 координата Y переданной точки
+ * @param xc                 координата X центра окружности
+ * @param yc                 координата Y центра окружности
+ * @param r                  радиус окружности
+ * @param thresholdReduction степень уменьшения порога вхождения точки в окружность
  */
 export const getIsPointInsideCircle = (
   xp: number,
   yp: number,
   xc: number,
   yc: number,
-  r: number
+  r: number,
+  thresholdReduction = BEAUTY_COEFFICIENT
 ) => {
   const d = Math.sqrt(Math.pow(xp - xc, 2) + Math.pow(yp - yc, 2));
 
-  return Math.pow(d, 2) <= Math.pow(r, 2);
+  return Math.pow(d - thresholdReduction, 2) <= Math.pow(r, 2);
 };
